9b2c3442b1d040ff04c6b8fdffd43890fd28a297
destruxio <tandeshmukh@gmail.com>
2020-07-31 22:10:38 -0400

    num letters

---
+++ python/math/problems_11-20/number_letter_counts.py
@@ -0,0 +1,17 @@
+# Word values
+
+ones = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+
+tens = [ "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+hundreds = [ "onehundred", "twohundred", "threehundred", "fourhundred", "fivehundred", "sixhundred", "sevenhundred", "eighthundred", "ninehundred"]
+
+thousand = "onethousand"
+
+num_lens = []
+
+# Ones
+
+for i in range(1,20):
+    
+    
